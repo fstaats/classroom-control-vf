2af434b7f7dcf71be5fb0ae2da599f943f7cbeac
fstaats <frederick.l.staats@intel.com>
2016-03-29 13:20:08 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,61 @@
+class nginx {
 
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+  
+  file { '/etc/nginx':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+  
+  file { '/etc/nginx/conf.d':
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+  
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  
+  file { '/var/www/index.html':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+  }
+  
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => [File['/etc/nginx/nginx.conf'], File['/etc/nginx/conf.d/default.conf']]
+  }
+
+}
