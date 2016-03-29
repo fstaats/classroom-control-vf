14be08a6035775389f242251def33838ca6048f4
fstaats <frederick.l.staats@intel.com>
2016-03-29 10:39:11 -0700

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,12 @@
 class skeleton {
   file { '/etc/skel':
-    ensure => 'directory',
+    ensure  => 'directory',
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
   }
   
-  file { '/etc/skel/.bashrc'
+  file { '/etc/skel/.bashrc':
     ensure  => 'file',
     owner   => 'root',
     group   => 'root',
